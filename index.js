9d3b29cf147f4a2a21b4b263954356aba165d7a2
Kyle <ktvitale7@gmail.com>
2022-05-08 16:36:16 -0400

    commit tree 911b8a3a05c54a954f23bfdd74113d3b42ed76f2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    ]
+   
+    // const inventory = [
+    //     {name: 'apples', quantity: 2},
+    //     {name: 'bananas', quantity: 0},
+    //     {name: 'cherries', quantity: 5}
+    //   ];
+      
+    //   const result = inventory.find( ({ name }) => name === 'cherries' );
+      
+    //   console.log(result) // { name: 'cherries', quantity: 5 }
+    
+    // Will loop through record
+    // Find the first Object that returns true based on a callback
+
+  function superbowlWin(record){
+      const result = record.find(record => record.result === "W")
+      return !!result ? result.year : undefined 
+  }
+//   condition ? exprIfTrue : exprIfFalse
+// Will find the first year the Broncos got a 'W'
